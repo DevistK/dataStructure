bc181375000870b32a325ee047dc1152ac7e1d1c
tjehdgur1500 <tjehdgur1500@naver.com>
2022-03-30 12:20:30 +0900

    ADD : 스택 push, pop 값 확인 로그 추가 pop 예외처리 : 스택이 비어있을때 isEmpty 구현

--- Stack.js
+++ Stack.js
@@ -18,36 +18,58 @@ class Stack {
     push = (data) => {
         let cur;
 
+        // 데이터가 하나도 없을때 top과 bottom의 노드값을 동일하게 함
+        // 스토리지에 데이터만 추가
         if (this.size === 0) {
             this.top = new Node(data);
             this.bottom = new Node(data);
             this.storage[this.size] = this.top.data
         } else {
+            // 새 노드로 스택을 쌓을때 이전 요소를 포인터로 연결
             cur = new Node(data);
             cur.next = this.top;
+            // 최근 추가된 노드로 top을 변경
             this.top = cur;
+            // 스토리지에 순서대로 데이터 추가
             this.storage[this.size] = this.top.data
         }
 
+        console.log(this.top.data);
+        // 사이즈 변경
         this.size++;
     }
 
     // 데이터 위에서부터 제거
     pop = () => {
+
+        if (this.isEmpty()){
+            return '제거할 노드가 없습니다.';
+        }
+
+        // 제거할 노드 임시 변수에 할당
         let popNode = this.top;
+        // 제거할 노드 다음노드를 가장 위 노드로 지정과 동시에 연결해제
         let cur = popNode.next;
         this.top = cur;
 
+        // size-1 을 해 popNode의 index와 맞춰 null로 값 할당
         this.storage[this.size-1] = null;
 
+        // 스토리지에서 null 이 아닌 요소만 리턴해 재할당
         this.storage = this.storage.filter((item)=> item !== null);
 
+        console.log(popNode.data);
+        // 사이즈 변경
         this.size--;
     }
 
     // 데이터가 비어있는지 확인
     isEmpty = () => {
-
+        if (this.size <= 0){
+            return true
+        }else{
+            return false;
+        }
     }
 
     // 마지막에 넣은 데이터 확인
