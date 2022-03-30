5296b9bb8af61e36df243e4b443df1f2fba9df44
tjehdgur1500 <tjehdgur1500@naver.com>
2022-03-30 12:11:04 +0900

    ADD : 스택 push , pop 구현

--- Stack.js
+++ Stack.js
@@ -11,15 +11,39 @@ class Stack {
         this.top = null; // 스택의 last
         this.bottom = null; // 스택의 first
         this.size = 0; // 전체 사이즈
+        this.storage = []; // 데이터를 배열 형태로 조회하고자할때 , 사용할지안할지는 나중에
     }
 
     // 데이터 쌓기
-    push = () => {
+    push = (data) => {
+        let cur;
 
+        if (this.size === 0) {
+            this.top = new Node(data);
+            this.bottom = new Node(data);
+            this.storage[this.size] = this.top.data
+        } else {
+            cur = new Node(data);
+            cur.next = this.top;
+            this.top = cur;
+            this.storage[this.size] = this.top.data
+        }
+
+        this.size++;
     }
 
     // 데이터 위에서부터 제거
-    pop = () => { }
+    pop = () => {
+        let popNode = this.top;
+        let cur = popNode.next;
+        this.top = cur;
+
+        this.storage[this.size-1] = null;
+
+        this.storage = this.storage.filter((item)=> item !== null);
+
+        this.size--;
+    }
 
     // 데이터가 비어있는지 확인
     isEmpty = () => {
@@ -27,7 +51,9 @@ class Stack {
     }
 
     // 마지막에 넣은 데이터 확인
-    peek = ()=>{
-        
+    peek = () => {
+
     }
 }
+
+const stack = new Stack();
