86d07baa5a372c50e2bdcca22222694f015994fa
tjehdgur1500 <tjehdgur1500@naver.com>
2022-03-30 11:32:21 +0900

    ADD : 스택 구현 뼈대 생성성

---
+++ Stack.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+class Stack {
+    constructor() {
+        this.top = null; // 스택의 last
+        this.bottom = null; // 스택의 first
+        this.size = 0; // 전체 사이즈
+    }
+
+    // 데이터 쌓기
+    push = () => {
+
+    }
+
+    // 데이터 위에서부터 제거
+    pop = () => { }
+
+    // 데이터가 비어있는지 확인
+    isEmpty = () => {
+
+    }
+
+    // 마지막에 넣은 데이터 확인
+    peek = ()=>{
+        
+    }
+}
