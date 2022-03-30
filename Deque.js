310fba69807d4fa5bc6f07f0575310d92e1aa8ce
tjehdgur1500 <tjehdgur1500@naver.com>
2022-03-30 17:59:55 +0900

    ADD : 덱 구현 시작

--- Deque.js
+++ Deque.js
@@ -5,3 +5,61 @@ class Node {
         this.next = next;
     }
 }
+
+class Deque {
+    constructor() {
+        this.head = null;
+        this.rear = null;
+        this.size = 0;
+        this.storage = [];
+    }
+
+    // 맨앞에 노드 추가
+    addFront = () => { }
+
+    // 맨뒤에 노드 추가
+    addRear = () => {
+
+    }
+
+    // 맨앞쪽 노드 제거
+    removeFront = () => {
+
+    }
+
+    // 맨뒤쪽 노드 제거
+    removeRear = () => {
+
+    }
+
+    // 맨 앞 노드 조회
+    getFront = () => {
+
+    }
+
+    // 맨 뒤 노드 조회
+    getRear = () => {
+
+    }
+
+    // 전체 사이즈 조회
+    getSize = () => {
+
+    }
+
+    // 큐가 비어있는지 확인
+    isEmpty = () => {
+
+    }
+
+    // 스토리지 조회
+    getStorage = () => {
+
+    }
+
+    // 리스트 조회
+    getList = () => {
+
+    }
+
+}
