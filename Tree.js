72ba8647597b4c913ce6efea859f704806db264b
tjehdgur1500 <tjehdgur1500@naver.com>
2022-04-02 22:52:40 +0900

    BST 노드추가 구현

--- Tree.js
+++ Tree.js
@@ -19,13 +19,26 @@ class BST {
             this.root = new Node(data);
         } else {
             // 루트 초기화 이후 노드 추가를 위한 재귀
-            let searchNode = (data) => {
+            let searchNode = (node) => {
                 if (data < node.data) {
                     // 새로 추가되는 노드가 더 작으면 왼쪽으로 가지가 뻗어나감
+                    if (node.left === null) {
+                        node.left = new Node(data);
+                        return true;
+                    } else if (node.left !== null) {
+                        return searchNode(node.left);
+                    }
                 } else if (data > node.data) {
                     // 새로 추가되는 노드가 더 크면 오른쪽으로 가지가 뻗어나감
+                    if (node.right === null) {
+                        node.right = new Node(data);
+                        return true;
+                    } else if (node.right !== null) {
+                        return searchNode(node.right);
+                    }
                 }
             };
+            return searchNode(node);
         }
     };
 }
