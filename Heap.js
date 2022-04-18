4abe3d4167174521004a84c4f98b35a01425e159
tjehdgur1500 <tjehdgur1500@naver.com>
2022-04-18 19:51:55 +0900

    [feat] 힙 구현 삭제 자식노드가 없을때와 1개만 있을떄 로직 추가

--- Heap.js
+++ Heap.js
@@ -9,7 +9,7 @@ class Heap {
     //  값이 더 크다면 서로 위치를 바꿔준다. ( swap 구현 )
 
     swap(currentIdx, parentIdx){
-
+        [this.heap[parentIdx], this.heap[currentIdx]] = [this.heap[currentIdx], this.heap[parentIdx]];
     }
 
     push(node){
@@ -19,13 +19,57 @@ class Heap {
         // 그 노드의 부모 인덱스 값
         let parentIdx = Math.floor(currentIdx / 2);
 
-        // while (currentIdx > 1 && this.heap[parentIdx] > this.heap[currentIdx]){
+        while (currentIdx > 1 && this.heap[parentIdx] > this.heap[currentIdx]){
         //   2번째로 들어오는 노드는 인덱스는 항상 1보다 크다.
         //   부모 노드가 현재 노드보다 더 크다면 스왑을 해야한다.
-        // }
-        this.swap(currentIdx, parentIdx);
+            this.swap(currentIdx, parentIdx);
+            // 스왑을 통해 바뀐 노드의 인덱스 조정 후 다시 조건에 부합하는지 반복
+            currentIdx = parentIdx;
+            parentIdx = Math.floor(currentIdx / 2);
+        }
+    }
+
+    pop(){
+        const size = this.heap.length;
+        // 루트 노드
+        const min =  this.heap[1];
+
+        // 제거 과정
+        if (size <= 2){
+            // 힙의 노드가 1개거나 아무것도 없다면
+            // 다시 null 로 채워 아무것도 없게끔함
+            // 2랑 같거나 작게한 이유는 인지부조화 방지를 위한 null 을 배열 맨 첫자리에 넣었기 때문
+            this.heap = [null];
+        }else{
+            // 최상단 루트를 제일 끝에있는 노드로 바꿈
+            this.heap[1] = this.heap.pop();
+        }
+
+        // 인덱스 재조정
+        let currentIdx = 1;
+        let leftIdx = Math.floor(currentIdx * 2);
+        let rightIdx = Math.floor((currentIdx * 2) + 1);
+
+        /*
+          1. 노드가 양쪽 둘다 없을떄
+          2. 노드가 왼쪽만 있을때
+          3. 노드가 양쪽 둘다 있을때
+        */
+
+        // 왼쪽부터 채워지는 트리구조에 따라 왼쪽이 없다면 양쪽둘다 없으므로 root 반환
+        if (!this.heap[leftIdx]) return min;
+
+        // 왼쪽만 존재한다면
+        if (!this.heap[rightIdx]){
+            // 왼쪽 노드값이 루트보다 작을때 스왑한다.
+            if (this.heap[leftIdx] < this.heap[currentIdx]){
+                this.swap(leftIdx, currentIdx);
+            }
+            return min;
+        }
     }
 
 }
 
 const heap = new Heap();
+heap.push(1);
